817c5587d53762f232d6c2bd1bd1b06bb788d292
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-10-07 09:39:29 -0500

    intro done

---
+++ src/Fundamentals/IntroNotes.java
@@ -0,0 +1,74 @@
+package Fundamentals;
+
+public class IntroNotes {
+//  NOTE: A computer program is a SET OF INSTRUCTIONS that run on a computer or other digtal device.
+//  Machine level - consist of binary instructions (1s and 0s) binary
+//  High-level code (readable) is what most programs are written in., this has to be tranlated to machine code before it can run it.
+
+//  NOTE:
+//  Compilers turns the high-level code into machine code.
+//  Compilers need to beable to speak the same language as the computer or platform.
+//  So you can't use a Linux compiler for a windows computer.
+// Compiler compiles the code into an executable file,
+//  NOTE: Libraries
+//  There's also project libraries that help simplfy the process, so you don't have recreate code/procedures over and over again
+//  Library files are linked to a specify program for a specfic system/platform
+
+//  NOTE: Java language is PLATFORM INDEPENDENT, unlike C and C++
+//  Which means it DOES NOT need ot be compiled 3 times for 3 different platforms, like the others would
+//  As long as you a JVM (Java Virtual Machine) installed on the computer it doesn't matter
+//  NOTE High-level to compiler to machine level
+
+//  NOTE: Java is the number 1 language in the world.
+//  JRE = Java run time evenviroment, this is used for END USERS to run applications
+//  JDK is used by developers Java development Kit (Could also be known as SDK(Software...)
+//  All non-smart phones, (feature phones?) run java
+//  JVM = JAva Virtual MAchine, (Used to run java right?)
+//  MSA API is the mobile Software APplication used to create user interfaces on portable devices.
+
+//  NOTE Java SE Stuff (standard edition)
+//  Is used to develop applets that run within web browsers and applications that run on desktops
+//  NOTE applets vs application
+//  Applets launched in the browser vs
+//  Applications are launched within an OS as a standalone program
+
+//  NOTE Java EE Stuff (Enterprise edition)
+//  Used to create LARGE enterprise, server-side, and client-side distributed applications
+//  Expands off of the SE platform, by extending it with more APIs (Application Programming interfaces) that support the needs of a large scale, high performance environment.
+
+//  NOTE JAVA ME Micro Edition
+//  Used to create applications for resourc-constrained consumer devices(Phones blu-ray players etc)
+
+//  NOTE Java Card
+//  Used to secure information, normally found on a smart card. Even mobile phone SIM cards
+
+//  NOTE PLC (Product Life Cycle) Stages
+
+//  notenum 1. Analysis
+//  Process of looking at a problem that you want to solve with your software., You want to CLEARLY Define the program you want to solve, or the application you want to build.
+//  The scope of the project, (or boundry of a program), is the big picture of every aspect of the software you want to develop.
+
+// notenum 2. Design
+// Process of applying the findings you made during the analysis stage to the design of th product
+//  Make the specifications for the products or components in your system.
+
+
+// notenum 3. Development
+// When we take those blueprints or specifications from the design stage to create the actual components.
+//
+// notenum 4. Testing
+// We are testing to ensure that the individual components, and the product as a whole meet the requirements.
+//  Usually done by a team of dedicated testers, who were NOT involved in the actual development of the product.
+//  This is done to make sure it's tested OBJECTIVELY, without any bias.
+// notenum 5. Implementation
+// Where we deliver the software product and make it available to customers.
+// notenum 6. Maintenance
+// Problems are fixed and the product is re-released through patches(for minor fixes or updates) and in the form of new versions when there are enough changes that a new version is merited.
+// notenum 7. End-of-Life (EOL)
+// Carry out all the essential task to ensure that the customers and employees are aware that a product is no longer being sold.
+
+
+
+
+
+}
