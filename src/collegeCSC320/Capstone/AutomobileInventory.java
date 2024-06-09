7475baa00adbe1c1ad781ca40f808f9090c03365
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2024-06-08 21:14:52 -0500

    Testing and Capstone complete

--- src/collegeCSC320/Capstone/AutomobileInventory.java
+++ src/collegeCSC320/Capstone/AutomobileInventory.java
@@ -1,5 +1,6 @@
 package collegeCSC320.Capstone;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -41,17 +42,6 @@ public class AutomobileInventory {
       System.out.println(listItem);
     }
   }
-//  public int findVin (string vin) {
-//Int carIndex = -1; //We want an invalid number here just incase
-//bool carFound = false;
-//for (int i = 0; i < getAutomobiles.size(); ++i) {
-//if ( getAutomobiles.get(i).getVin == vin) {
-//carIndex = i;
-//carFound = true;
-//i = getAutomobiles.size() // should end for loop? } end if
-//} end for loop
-//return carIndex
-//} end of findVin
 
   public int findVin(String vin) {
     int carIndex = -1;
@@ -73,26 +63,76 @@ public class AutomobileInventory {
     }
     return carIndex;
   }
-  //public string addAuto (string make, string model, string vin, string color, int year, int mileage, double price) {
-  //Automobile newCar = new Automobile(make, model, vin, color, year, mileage, price)
-  //automobiles.add(newCar)
-  //listInventory()
-  //Return “Vehicle Successfully Added”
-  //}
 
   public String addAutomobile (String make, String model, String vin, String color, int year, int mileage, double price) {
     String message = "";
     try {
       Automobile newCar = new Automobile(make, model, vin, color, year, mileage, price);
       automobiles.add(newCar);
-      message = "Automobile successfully Added";
+      message = "Automobile successfully Added \n";
     } catch (Exception ex){
       message = "There was an issue adding your car. Error below: \n" + ex.toString();
     }
     return message;
   }
 
+  public String deleteAutomobile(String vin) {
+    int vinIndex = findVin(vin);
+    String output = "";
+    try {
+      if (vinIndex == -1) {
+        output = vin + " not found, please check the list to ensure you entered the correct vin.\n";
+      } else {
+        this.getAutomobiles().remove(vinIndex);
+        output =  vin + " was deleted.\n";
+      }
+    } catch (Exception ex ) {
+      output = "Trying to delete: " + vin + ". ran into an issue exception information below.\n" + ex.toString();
+    }
+    return output;
+  }
+
+  public String updateAutomobile (String make, String model, String vin, String color, int year, int mileage, double price) {
+    int vinIndex = findVin(vin);
+    String output = "";
+
+    try {
+      if (vinIndex == -1) {
+        output = vin + " not found, please check the list to ensure you entered the correct vin.\n";
+      } else {
+        Automobile update = new Automobile(make, model, vin, color, year, mileage, price);
+        automobiles.set(vinIndex, update);
+        output = vin + " successfully updated.";
+      }
+    } catch (Exception ex) {
+      output = "There was an issue updating " + vin + ".\n Exception information below.\n" + ex.toString();
+    }
+
+    return output;
+  }
+
+  public void writeToFile (AutomobileInventory writeThis ) {
+
+    StringBuilder inventoryList = new StringBuilder();
+    for (String listItem : this.listInventory()){
+      inventoryList.append(listItem);
+    }
+
+    try {
+      //Note: the path is for my MAC, you will need to edit this depending on your own path.
+      File newTextFile = new File("/Users/joshuawilliams/Test/Autos.txt");
+
+      FileWriter fw = new FileWriter(newTextFile);
+      fw.write(inventoryList.toString());
+      fw.close();
+
+    } catch (IOException ex) {
+      System.out.println("There was an issue printing the file: \n" + ex.toString());
+    }
+  }
+
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     //Setting up test data
     Automobile testA = new Automobile("AIK", "Amod", "AAAAA", "Acolor", 1000, 111, 1900.10 );
     Automobile testB = new Automobile("Bord", "Bodel", "BBBBB", "Bcolor", 2000, 222, 2800.20 );
@@ -115,14 +155,62 @@ public class AutomobileInventory {
     //Test findVin
     int expectedInt = 2;
     if (testInventory.findVin("ccccc") == expectedInt) {
-      System.out.println("Test Successful");
+      System.out.println("findVin Test Successful\n");
+    } else {
+      System.out.println("Test findVin Failed");
     }
     //End Test findVin
 
     //Test addAutomobile
-    testInventory.addAutomobile("Dord", "Dunder Bird", "DDDDD", "Dcolor", 4000, 444, 4600.40);
-    testInventory.displayInventory();
+    String addAutoTest = testInventory.addAutomobile("Doge", "Dunder Bird", "DDDDD", "Dcolor", 4000, 444, 4600.40);
+    if (testInventory.getAutomobiles().size() == 4) {
+      System.out.println(addAutoTest);
+      testInventory.displayInventory();
+    } else {
+      System.out.println("Test addAutomobile Failed");
+    }
     //End Test addAutomobile
+
+    //Test deleteAutomobile
+    String deleteAutoTest = testInventory.deleteAutomobile( "CCCCC");
+    if (testInventory.getAutomobiles().size() == 3) {
+      System.out.println(deleteAutoTest);
+      testInventory.displayInventory();
+    } else {
+      System.out.println("Test deleteAutomobile Failed");
+    }
+    //End Test deleteAutomobile
+
+    //Test updateAutomobile
+    String updateAutoTest = testInventory.updateAutomobile("Bodge", "Bunder Bird", "BBBBB", "Better Color", 2014, 500, 4999.40);
+    if(testInventory.getAutomobiles().get(1).getYear() == 2014) {
+      testInventory.displayInventory();
+    } else {
+      System.out.println("Test updateAutomobile Failed");
+    }
+    //End Test updateAutomobile
+
+    //Write to file
+    String response = "";
+    while(!response.equalsIgnoreCase("y") || !response.equalsIgnoreCase("n") ) {
+      System.out.println("Would you like to print this information to a file? (Y or N)");
+      response = scanner.next();
+      if (response.equalsIgnoreCase("y")) {
+        System.out.println("Saving to: C:\\Temp\\Autos.txt ");
+        try {
+          testInventory.writeToFile(testInventory);
+        } catch (Exception ex ){
+          System.out.println("Issue saving data.");
+        }
+        break;
+      } else if (response.equalsIgnoreCase("n")) {
+        System.out.println("Goodbye.");
+        break;
+      } else {
+        System.out.println("Invalid response given");
+      }
+    }
+    //End Write to file
   }
 
 }
