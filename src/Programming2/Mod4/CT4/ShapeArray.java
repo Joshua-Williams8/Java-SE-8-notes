480c937f29c7c9db7d7478ba9bef5acaa5c9fd13
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2025-01-11 15:11:21 -0600

    minor change to driver

--- src/Programming2/Mod4/CT4/ShapeArray.java
+++ src/Programming2/Mod4/CT4/ShapeArray.java
@@ -5,7 +5,7 @@ public class ShapeArray {
   public static void main(String[] args) {
 
     Triangle tri = new Triangle(5,4,5);
-    Circle cir = new Circle(5);
+    Circle cir = new Circle(9);
     Rectangle rec = new Rectangle(5,5);
 
     Shape[] shapeArray = {tri, cir, rec};
